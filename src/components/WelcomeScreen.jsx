c2407d3fc3c49b2c112371f0306a87f183600e88
Theja1905 <153060692+Theja1905@users.noreply.github.com>
2025-08-27 23:47:17 +0800

    your message

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,28 +1,28 @@
-import { view, text } from "@lynx-js/react";
-import Avatar from "./Avatar.jsx";
+import { view, text, image } from "@lynx-js/react"; // Add image import
 import "../styles/WelcomeScreen.css";
+import welcomeImage from "../assets/welcome.png";
 
 export default function WelcomeScreen({ onContinue }) {
   return (
     <view className="welcome-screen">
       <view className="welcome-content">
-        <view className="welcome-avatar">
-          <Avatar size="large" showName={true} />
-        </view>
-        
         <view className="welcome-messages">
-          <text className="welcome-title">Hello! I'm Lumi </text>
+          <text className="welcome-title">Hello!</text>
+          <text className="welcome-title">Welcome to Lumi</text>
           <text className="welcome-description">
-            I'm your friendly AI assistant, here to help you with anything you need! 
-            From answering questions to having conversations, I'm excited to chat with you.
-          </text>
-          <text className="welcome-sub">
-            Let's get you set up so we can start our journey together!
+            Your Personal Mindful Companion
           </text>
         </view>
 
         <view className="welcome-button" bindtap={onContinue}>
           <text>Get Started</text>
+        </view>
+        <view className="welcome-bunny-container">
+          <image 
+            src={welcomeImage}
+            className="welcome-bunny" 
+            mode="aspectFit"
+          />
         </view>
       </view>
     </view>
